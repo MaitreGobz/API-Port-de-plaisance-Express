d2ab25f5a02d0c76e18a4ac1b63905f9ec30b640
MaitreGobz <nayetlucas@hotmail.fr>
2025-10-08 16:55:59 +0200

    save of reservation model

---
+++ models/Reservation.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema({
+    catwayNumber: {
+        type: Number,
+        required: true,
+        index: true
+    },
+    clientName: {
+        type: String,
+        required: true
+    },
+    boatName: {
+        type: String,
+        require: true
+    },
+    checkIn: {
+        type: Date,
+        required: true
+    },
+    checkOut: {
+        type: Date,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Reservation', reservationSchema);
